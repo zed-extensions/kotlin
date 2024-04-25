5b1267c7fec10e6db6c92f53662b901cdc3968d2
Guest user <158013953+spaceunifyfifty@users.noreply.github.com>
2024-04-25 14:11:27 +0000

    add: brackets.scm for bracket pair highlighting

---
+++ languages/kotlin/brackets.scm
@@ -0,0 +1,5 @@
+("(" @open ")" @close)
+("[" @open "]" @close)
+("{" @open "}" @close)
+("<" @open ">" @close)
+; ("\"" @open "\"" @close) FIXME: `Invalid node type`. This line exists in the `brackets.scm` for other languages, but errors here.
