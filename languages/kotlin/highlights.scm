2874a440fe3bd78d3dc8cd584151026022ce900b
Guest user <158013953+spaceunifyfifty@users.noreply.github.com>
2024-05-16 14:11:56 +0000

    convert tabs to spaces
    context: https://github.com/zed-extensions/kotlin/pull/7#discussion_r1603411015

--- languages/kotlin/highlights.scm
+++ languages/kotlin/highlights.scm
@@ -27,67 +27,67 @@
 (super_expression) @variable.builtin
 
 (class_parameter
-	(simple_identifier) @property)
+  (simple_identifier) @property)
 
 (class_body
-	(property_declaration
-		(variable_declaration
-			(simple_identifier) @property)))
+  (property_declaration
+    (variable_declaration
+      (simple_identifier) @property)))
 
 ; id_1.id_2.id_3: `id_2` and `id_3` are assumed as object properties
 (_
-	(navigation_suffix
-		(simple_identifier) @property))
+  (navigation_suffix
+    (simple_identifier) @property))
 
 (enum_entry
-	(simple_identifier) @constant)
+  (simple_identifier) @constant)
 
 (type_identifier) @type
 
 ((type_identifier) @type.builtin
-	(#any-of? @type.builtin
-		"Byte"
-		"Short"
-		"Int"
-		"Long"
-		"UByte"
-		"UShort"
-		"UInt"
-		"ULong"
-		"Float"
-		"Double"
-		"Boolean"
-		"Char"
-		"String"
-		"Array"
-		"ByteArray"
-		"ShortArray"
-		"IntArray"
-		"LongArray"
-		"UByteArray"
-		"UShortArray"
-		"UIntArray"
-		"ULongArray"
-		"FloatArray"
-		"DoubleArray"
-		"BooleanArray"
-		"CharArray"
-		"Map"
-		"Set"
-		"List"
-		"EmptyMap"
-		"EmptySet"
-		"EmptyList"
-		"MutableMap"
-		"MutableSet"
-		"MutableList"
+  (#any-of? @type.builtin
+    "Byte"
+    "Short"
+    "Int"
+    "Long"
+    "UByte"
+    "UShort"
+    "UInt"
+    "ULong"
+    "Float"
+    "Double"
+    "Boolean"
+    "Char"
+    "String"
+    "Array"
+    "ByteArray"
+    "ShortArray"
+    "IntArray"
+    "LongArray"
+    "UByteArray"
+    "UShortArray"
+    "UIntArray"
+    "ULongArray"
+    "FloatArray"
+    "DoubleArray"
+    "BooleanArray"
+    "CharArray"
+    "Map"
+    "Set"
+    "List"
+    "EmptyMap"
+    "EmptySet"
+    "EmptyList"
+    "MutableMap"
+    "MutableSet"
+    "MutableList"
 ))
 
 (package_header
-	. (identifier)) @namespace
+  . (identifier)) @namespace
 
 (import_header
-	"import" @include)
+  "import" @include)
 
 
 ; TODO: Seperate labeled returns/breaks/continue/super/this
@@ -97,116 +97,116 @@
 ;;; Function definitions
 
 (function_declaration
-	. (simple_identifier) @function)
+  . (simple_identifier) @function)
 
 (getter
-	("get") @function.builtin)
+  ("get") @function.builtin)
 (setter
-	("set") @function.builtin)
+  ("set") @function.builtin)
 
 (primary_constructor) @constructor
 (secondary_constructor
-	("constructor") @constructor)
+  ("constructor") @constructor)
 
 (constructor_invocation
-	(user_type
-		(type_identifier) @constructor))
+  (user_type
+    (type_identifier) @constructor))
 
 (anonymous_initializer
-	("init") @constructor)
+  ("init") @constructor)
 
 (parameter
-	(simple_identifier) @parameter)
+  (simple_identifier) @parameter)
 
 (parameter_with_optional_type
-	(simple_identifier) @parameter)
+  (simple_identifier) @parameter)
 
 ; lambda parameters
 (lambda_literal
-	(lambda_parameters
-		(variable_declaration
-			(simple_identifier) @parameter)))
+  (lambda_parameters
+    (variable_declaration
+      (simple_identifier) @parameter)))
 
 ;;; Function calls
 
 ; function()
 (call_expression
-	. (simple_identifier) @function)
+  . (simple_identifier) @function)
 
 ; object.function() or object.property.function()
 (call_expression
-	(navigation_expression
-		(navigation_suffix
-			(simple_identifier) @function) . ))
+  (navigation_expression
+    (navigation_suffix
+      (simple_identifier) @function) . ))
 
 (call_expression
-	. (simple_identifier) @function.builtin
-	(#any-of? @function.builtin
-		"arrayOf"
-		"arrayOfNulls"
-		"byteArrayOf"
-		"shortArrayOf"
-		"intArrayOf"
-		"longArrayOf"
-		"ubyteArrayOf"
-		"ushortArrayOf"
-		"uintArrayOf"
-		"ulongArrayOf"
-		"floatArrayOf"
-		"doubleArrayOf"
-		"booleanArrayOf"
-		"charArrayOf"
-		"emptyArray"
-		"mapOf"
-		"setOf"
-		"listOf"
-		"emptyMap"
-		"emptySet"
-		"emptyList"
-		"mutableMapOf"
-		"mutableSetOf"
-		"mutableListOf"
-		"print"
-		"println"
-		"error"
-		"TODO"
-		"run"
-		"runCatching"
-		"repeat"
-		"lazy"
-		"lazyOf"
-		"enumValues"
-		"enumValueOf"
-		"assert"
-		"check"
-		"checkNotNull"
-		"require"
-		"requireNotNull"
-		"with"
-		"suspend"
-		"synchronized"
+  . (simple_identifier) @function.builtin
+  (#any-of? @function.builtin
+    "arrayOf"
+    "arrayOfNulls"
+    "byteArrayOf"
+    "shortArrayOf"
+    "intArrayOf"
+    "longArrayOf"
+    "ubyteArrayOf"
+    "ushortArrayOf"
+    "uintArrayOf"
+    "ulongArrayOf"
+    "floatArrayOf"
+    "doubleArrayOf"
+    "booleanArrayOf"
+    "charArrayOf"
+    "emptyArray"
+    "mapOf"
+    "setOf"
+    "listOf"
+    "emptyMap"
+    "emptySet"
+    "emptyList"
+    "mutableMapOf"
+    "mutableSetOf"
+    "mutableListOf"
+    "print"
+    "println"
+    "error"
+    "TODO"
+    "run"
+    "runCatching"
+    "repeat"
+    "lazy"
+    "lazyOf"
+    "enumValues"
+    "enumValueOf"
+    "assert"
+    "check"
+    "checkNotNull"
+    "require"
+    "requireNotNull"
+    "with"
+    "suspend"
+    "synchronized"
 ))
 
 ;;; Literals
 
 [
-	(line_comment)
-	(multiline_comment)
-	(shebang_line)
+  (line_comment)
+  (multiline_comment)
+  (shebang_line)
 ] @comment
 
 (real_literal) @float
 [
-	(integer_literal)
-	(long_literal)
-	(hex_literal)
-	(bin_literal)
-	(unsigned_literal)
+  (integer_literal)
+  (long_literal)
+  (hex_literal)
+  (bin_literal)
+  (unsigned_literal)
 ] @number
 
 [
-	"null" ; should be highlighted the same as booleans
-	(boolean_literal)
+  "null" ; should be highlighted the same as booleans
+  (boolean_literal)
 ] @boolean
 
 (character_literal) @character
@@ -218,67 +218,67 @@
 ; There are 3 ways to define a regex
 ;	- "[abc]?".toRegex()
 (call_expression
-	(navigation_expression
-		((string_literal) @string.regex)
-		(navigation_suffix
-			((simple_identifier) @_function
-			(#eq? @_function "toRegex")))))
+  (navigation_expression
+    ((string_literal) @string.regex)
+    (navigation_suffix
+      ((simple_identifier) @_function
+      (#eq? @_function "toRegex")))))
 
 ;	- Regex("[abc]?")
 (call_expression
-	((simple_identifier) @_function
-	(#eq? @_function "Regex"))
-	(call_suffix
-		(value_arguments
-			(value_argument
-				(string_literal) @string.regex))))
+  ((simple_identifier) @_function
+  (#eq? @_function "Regex"))
+  (call_suffix
+    (value_arguments
+      (value_argument
+        (string_literal) @string.regex))))
 
 ;	- Regex.fromLiteral("[abc]?")
 (call_expression
-	(navigation_expression
-		((simple_identifier) @_class
-		(#eq? @_class "Regex"))
-		(navigation_suffix
-			((simple_identifier) @_function
-			(#eq? @_function "fromLiteral"))))
-	(call_suffix
-		(value_arguments
-			(value_argument
-				(string_literal) @string.regex))))
+  (navigation_expression
+    ((simple_identifier) @_class
+    (#eq? @_class "Regex"))
+    (navigation_suffix
+      ((simple_identifier) @_function
+      (#eq? @_function "fromLiteral"))))
+  (call_suffix
+    (value_arguments
+      (value_argument
+        (string_literal) @string.regex))))
 
 ;;; Keywords
 
 (type_alias "typealias" @keyword)
 [
-	(class_modifier)
-	(member_modifier)
-	(function_modifier)
-	(property_modifier)
-	(platform_modifier)
-	(variance_modifier)
-	(parameter_modifier)
-	(visibility_modifier)
-	(reification_modifier)
-	(inheritance_modifier)
+  (class_modifier)
+  (member_modifier)
+  (function_modifier)
+  (property_modifier)
+  (platform_modifier)
+  (variance_modifier)
+  (parameter_modifier)
+  (visibility_modifier)
+  (reification_modifier)
+  (inheritance_modifier)
 ]@keyword
 
 [
-	"if"
-	"else"
-	"when"
-	"for"
-	"while"
-	"do"
-	"try"
-	"catch"
-	"throw"
-	"finally"
-	"val"
-	"var"
-	"enum"
-	"class"
-	"object"
-	"interface"
+  "if"
+  "else"
+  "when"
+  "for"
+  "while"
+  "do"
+  "try"
+  "catch"
+  "throw"
+  "finally"
+  "val"
+  "var"
+  "enum"
+  "class"
+  "object"
+  "interface"
 ;	"typeof" ; NOTE: It is reserved for future use
 ] @keyword
 
@@ -287,84 +287,84 @@
 (jump_expression) @keyword.return
 
 (annotation
-	"@" @attribute (use_site_target)? @attribute)
+  "@" @attribute (use_site_target)? @attribute)
 (annotation
-	(user_type
-		(type_identifier) @attribute))
+  (user_type
+    (type_identifier) @attribute))
 (annotation
-	(constructor_invocation
-		(user_type
-			(type_identifier) @attribute)))
+  (constructor_invocation
+    (user_type
+      (type_identifier) @attribute)))
 
 (file_annotation
-	"@" @attribute "file" @attribute ":" @attribute)
+  "@" @attribute "file" @attribute ":" @attribute)
 (file_annotation
-	(user_type
-		(type_identifier) @attribute))
+  (user_type
+    (type_identifier) @attribute))
 (file_annotation
-	(constructor_invocation
-		(user_type
-			(type_identifier) @attribute)))
+  (constructor_invocation
+    (user_type
+      (type_identifier) @attribute)))
 
 ;;; Operators & Punctuation
 
 [
-	"!"
-	"!="
-	"!=="
-	"="
-	"=="
-	"==="
-	">"
-	">="
-	"<"
-	"<="
-	"||"
-	"&&"
-	"+"
-	"++"
-	"+="
-	"-"
-	"--"
-	"-="
-	"*"
-	"*="
-	"/"
-	"/="
-	"%"
-	"%="
-	"?."
-	"?:"
-	"!!"
-	"is"
-	"!is"
-	"in"
-	"!in"
-	"as"
-	"as?"
-	".."
-	"->"
+  "!"
+  "!="
+  "!=="
+  "="
+  "=="
+  "==="
+  ">"
+  ">="
+  "<"
+  "<="
+  "||"
+  "&&"
+  "+"
+  "++"
+  "+="
+  "-"
+  "--"
+  "-="
+  "*"
+  "*="
+  "/"
+  "/="
+  "%"
+  "%="
+  "?."
+  "?:"
+  "!!"
+  "is"
+  "!is"
+  "in"
+  "!in"
+  "as"
+  "as?"
+  ".."
+  "->"
 ] @operator
 
 [
-	"(" ")"
-	"[" "]"
-	"{" "}"
+  "(" ")"
+  "[" "]"
+  "{" "}"
 ] @punctuation.bracket
 
 [
-	"."
-	","
-	";"
-	":"
-	"::"
+  "."
+  ","
+  ";"
+  ":"
+  "::"
 ] @punctuation.delimiter
 
 ; NOTE: `interpolated_identifier`s can be highlighted in any way
 (string_literal
-	"$" @punctuation.special
-	(interpolated_identifier) @none)
+  "$" @punctuation.special
+  (interpolated_identifier) @none)
 (string_literal
-	"${" @punctuation.special
-	(interpolated_expression) @none
-	"}" @punctuation.special)
+  "${" @punctuation.special
+  (interpolated_expression) @none
+  "}" @punctuation.special)
