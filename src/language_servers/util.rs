7871256205ba33bb4d34fd539b7fa82ccb05364e
MrSubidubi <dev@bahn.sh>
2026-05-06 01:09:25 +0200

    Remove for now

--- src/language_servers/util.rs
+++ src/language_servers/util.rs
@@ -17,19 +17,3 @@ pub(super) fn remove_outdated_versions(
     }
     Ok(())
 }
-
-pub(super) fn find_existing_binary(
-    language_server_id: &'static str,
-    binary_name: &str,
-) -> Option<String> {
-    fs::read_dir(".").ok()?.flatten().find_map(|entry| {
-        let binary_path = entry.path().join(binary_name);
-
-        (binary_path.is_file()
-            && entry
-                .file_name()
-                .to_str()
-                .is_some_and(|binary_dir| binary_dir.starts_with(language_server_id)))
-        .then(|| binary_path.to_string_lossy().to_string())
-    })
-}
